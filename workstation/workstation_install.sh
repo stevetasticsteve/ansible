e3aa0cf313935f6bc3f960bd0087785802edacad
steve <stevetasticsteve@gmail.com>
2021-02-03 17:45:16 +0000

    improvements

--- workstation/workstation_install.sh
+++ workstation/workstation_install.sh
@@ -2,5 +2,5 @@
 
 sudo apt install git ansible -y
 git clone https://github.com/stevetasticsteve/ansible
-cd ~Documents/ansible/
+cd ~/Documents/ansible/
 ansible-playbook ./workstation/steve_laptop.yml -K 
