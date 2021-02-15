41659098ac618bf587b8aa31ad6414f48f46bfc4
steve <stevetasticsteve@gmail.com>
2021-02-15 13:02:07 +0000

    improvements

--- workstation/workstation_install.sh
+++ workstation/workstation_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-sudo apt install git ansible -y
-cd ~/Documents
-git clone https://github.com/stevetasticsteve/ansible
-cd ~/Documents/ansible/
-ansible-playbook ./workstation/steve_laptop.yml -K 
+read -p "Set up for (s)teve or (g)erdine?: " user 
+sudo apt install ansible -y
+curl https://github.com/stevetasticsteve/ansible/archive/master.zip -L -o /tmp/ansible.zip
+unzip /tmp/ansible.zip -d /tmp/ansible
+if [[ $user == s ]]; then
+    ansible-playbook /tmp/ansible/ansible-master/workstation/steve_laptop.yml -K 
+elif [[ $user == g ]]; then
+    ansible-playbook /tmp/ansible/ansible-master/workstation/gerdine_laptop.yml -K 
+else
+    echo "Incorrect user, run the playbook yourself." && exit 1
+fi
+sudo dpkg-reconfigure libdvd-pkg
