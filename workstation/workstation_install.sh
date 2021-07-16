4d6fb6b21dc38313d9b46b1af76a92a7a769882e
steve <stevetasticsteve@gmail.com>
2021-07-16 11:46:49 +0100

    Added pip packages

--- workstation/workstation_install.sh
+++ workstation/workstation_install.sh
@@ -5,11 +5,11 @@ sudo apt install ansible curl -y
 curl https://github.com/stevetasticsteve/ansible/archive/master.zip -L -o /tmp/ansible.zip
 sudo rm -r /tmp/ansible
 unzip /tmp/ansible.zip -d /tmp/ansible
-cd /tmp/ansible/ansible-master
+cd /tmp/ansible/ansible-master/workstation
 if [[ $user == s ]]; then
-    ansible-playbook /tmp/ansible/ansible-master/workstation/steve_laptop.yml -K 
+    ansible-playbook /tmp/ansible/ansible-master/workstation/steve_laptop.yml
 elif [[ $user == g ]]; then
-    ansible-playbook /tmp/ansible/ansible-master/workstation/gerdine_laptop.yml -K 
+    ansible-playbook /tmp/ansible/ansible-master/workstation/gerdine_laptop.yml
 else
     echo "Incorrect user, run the playbook yourself." && exit 1
 fi
