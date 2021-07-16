dce28626af1fb9bdeb503943838fe94c530cc842
steve <stevetasticsteve@gmail.com>
2021-07-16 12:50:01 +0100

    forgot secrets password

--- workstation/workstation_install.sh
+++ workstation/workstation_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# wget https://raw.githubusercontent.com/stevetasticsteve/ansible/master/workstation/workstation_install.sh; bash workstation_install.sh
 
 read -p "Set up for (s)teve or (g)erdine?: " user 
 sudo apt install ansible curl -y
@@ -7,9 +8,9 @@ sudo rm -r /tmp/ansible
 unzip /tmp/ansible.zip -d /tmp/ansible
 cd /tmp/ansible/ansible-master/workstation
 if [[ $user == s ]]; then
-    ansible-playbook /tmp/ansible/ansible-master/workstation/steve_laptop.yml
+    ansible-playbook --ask-vault-pass steve_laptop.yml -K
 elif [[ $user == g ]]; then
-    ansible-playbook /tmp/ansible/ansible-master/workstation/gerdine_laptop.yml
+    ansible-playbook --ask-vault-pass gerdine_laptop.yml -K
 else
     echo "Incorrect user, run the playbook yourself." && exit 1
 fi
